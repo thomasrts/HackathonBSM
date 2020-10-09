b96e491d95da2d1e05f7f6f6d8dc5327b7eddf1d
Thomas <th.rotsaert@gmail.com>
2020-10-09 23:12:05 +0200

    v 0.0.2 09/10/20 23:11

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+require_once "Modele/header.php";
+$header = new header();
+?>
+
 <!doctype html>
 <html lang="fr">
 <head>
@@ -5,9 +10,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/bootstrap.bundle.js"></script>
+    <title>MOPODES</title>
 </head>
 <body>
-
+<header><?php $header->getheader()?></header>
 </body>
 </html>
