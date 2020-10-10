e58e85b2934f416cbe2690f2e394f4262bf197bc
Thomas <th.rotsaert@gmail.com>
2020-10-10 15:42:55 +0200

    v 0.1 10/10/20 15:42

--- Controller/connexion.php
+++ Controller/connexion.php
@@ -1,10 +1,8 @@
 <?php
-
-require_once "Modele/header.php";
-require_once "Modele/footer.php";
-require_once "Modele/functions.php";
-require_once "Modele/getlinks.php";
-$functions = new functions();
+require_once "../Modele/header.php";
+require_once "../Modele/footer.php";
+$header = new header();
+$footer = new footer();
 ?>
 
 <!doctype html>
@@ -14,24 +12,22 @@ $functions = new functions();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <?php $getlinks = new getlinks();
-    $getlinks->getlinks(); ?>
-    <title>Les Fripouilles - Connexion</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/Chart.css">
+    <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/Chart.bundle.js"></script>
+    <title>MOPODES - Connexion</title>
 </head>
 <body>
 <header>
-    <?php getheader(); ?>
+    <?php $header->getheader(); ?>
 </header>
-<?php
-$email = $_POST['email'];
-$mdp = $_POST['mdp'];
-$md5mdp = md5($mdp);
-?>
-<form style='margin: 15px;' method='post' action="enregistrer-infos-enfant.php">
+<form style='margin: 15px;' method='post'>
     <div class='container'>
         <div class="row" style="margin-bottom: 35px;">
             <div class="col-md-6" style="border-right: 1px solid #333">
-                <h4>Se connecter au RAM Les Fripouilles</h4>
+                <h4>Se connecter au projet MOPODES</h4>
                 <div class="form-group">
                     <label style='width: 150px ; margin-bottom:10px;'>Email : <br/><input id="$nom" type='text' required
                                                                                           placeholder="example@gmail.com"
@@ -45,45 +41,14 @@ $md5mdp = md5($mdp);
                 <button type="submit" name='envoi' style='border-top:25px' class="btn btn-primary">Valider</button>
             </div>
             <div class="col-md-5" style="margin-left: 20px;">
-                <h4>Pas encore inscrit au RAM Les Fripouilles ?</h4>
+                <h4>Pas encore inscrit au projet MOPODES ?</h4>
                 <a href="inscription.php" class="btn btn-primary" style="margin-top: 145px; margin-right:25px;">S'inscrire
-                    au RAM</a>
+                    au MOPODES</a>
             </div>
         </div>
-        <hr>
-        <h4>Vous inscrire au relais d'assistants maternelles Les Fripouilles c'est : </h4>
-        <ul style="margin-left:25px;">
-            <li>Un accueil personnalisé pour vous et votre enfant</li>
-            <li>Des activités adaptés pour tous</li>
-            <li>Un centre administratif pour faciliter vos démarches</li>
-            <li>Du personnel qualifié prêt à vous écouter</li>
-        </ul>
-    </div>
-    <?php
 
-    if (isset($_POST['envoi'])) {
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $md5_verif = $functions->verif_mdp($email);
-            if ($md5mdp == $md5_verif) {
-                setcookie('connection', $email, -1);
-                $id = $functions->getid($email);
-            } else {
-                if (isset($_COOKIE['connection'])) {
-                    unset($_COOKIE['connection']);
-                }
-                echo "<script language='JavaScript'>";
-                echo "alert('L'adresse mail et/ou le mot de passe que vous avez renseignés ne sont pas corrects, veuillez réessayer')";
-                echo "</script>";
-                exit;
-            }
-        }
-    }
-    ?>
 </form>
-
-
 <footer>
-    <?php getfooter(); ?>
 </footer>
 </body>
 </html>
--- Modele/header.php
+++ Modele/header.php
@@ -6,7 +6,7 @@ class header
 public static function getheader(){
     echo <<<_END
     <nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-bottom: 25px;">
-        <img src="./img/logo-nature-libre.png" width="8%" height="8%" style="margin-left: 25px;">
+        <img src="../img/logo-nature-libre.png" width="8%" height="8%" style="margin-left: 25px;">
         <a class="navbar-brand" href="../index.php" style="margin-left:20px;padding-left:25px; padding-right:25px; border-left:1px solid #333; border-right: 1px solid #333;">MOPODES</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -37,8 +37,8 @@ public static function getheader(){
                 <li class="nav-item">
                     <a class="nav-link" href="../edt.php" style="margin:15px;">Activités de la semaine</a>
                 </li>
-                <a href="Controller/inscription.php" class="btn btn-primary" style="margin: 10px; " >S'inscrire au RAM</a>
-                <a href="Controller/connexion.php" class="btn btn-primary" style="margin: 10px; margin-right:25px;">Se connecter au RAM</a>
+                <a href="Controller/inscription.php" class="btn btn-primary" style="margin: 10px; " >S'inscrire à MOPODES</a>
+                <a href="Controller/connexion.php" class="btn btn-primary" style="margin: 10px; margin-right:25px;">Se connecter à MOPODES</a>
             </ul>
         </div>
     </nav>
